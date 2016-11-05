05361401dd6897580d3b6b9f3d764f08e680543b
Pascal <jauffret@etud.insa-toulouse.fr>
2016-11-06 00:31:22 +0100

    Stayed on localhost, added override in bower.json for elasticsearch.angular.js, handled CORS by staying on localhost for the moment

--- Webapp/test/karma.conf.js
+++ Webapp/test/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/elasticsearch/elasticsearch.js',
+      'bower_components/elasticsearch/elasticsearch.angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
