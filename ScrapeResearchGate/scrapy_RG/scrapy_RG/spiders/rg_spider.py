c4716a9893ec54f8e3007692e3586a8669684677
Vito Mandorino <vitomandorino@MacBook-de-Vito-Mandorino.local>
2016-11-23 00:58:27 +0100

    further scraping

--- ScrapeResearchGate/scrapy_RG/scrapy_RG/spiders/rg_spider.py
+++ ScrapeResearchGate/scrapy_RG/scrapy_RG/spiders/rg_spider.py
@@ -29,7 +29,7 @@ class QuotesSpider(scrapy.Spider):
     name = "researchGate"
 
     custom_settings = {
-        'DOWNLOAD_DELAY': 10.0,
+        'DOWNLOAD_DELAY': 12.0,
         'RANDOMIZE_DOWNLOAD_DELAY': True,
         'AUTOTHROTTLE_ENABLED': True,
         'AUTOTHROTTLE_START_DELAY': 5.0,
@@ -39,7 +39,7 @@ class QuotesSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        urls_file = open('../urls_institutions.txt','r')
+        urls_file = open('../urls_french_institutions_partial.txt','r')
         urls = urls_file.readlines() 
         for url in urls:
             url = url.strip() + '/members' 
@@ -64,33 +64,38 @@ class QuotesSpider(scrapy.Spider):
 ##############################################
 
     def parse_institution_members(self, response):
-        institution = response.xpath('//span[@itemprop="name"]/text()').extract_first()
+        institution = response.xpath('//span[@itemprop="name"]/text()').extract_first().encode('utf-8')
+        institution_url = re.sub(r'\/members\/*\d*$','',response.url)
         location = response.xpath('//div[@itemprop="location"]/text()').extract_first()
         print "parse institution members"
         print "instit: " + institution
         print "location: " + location
         match = re.match(r'^(.*), *France\s*$', location) # add this if you want to restrict to French institutions only
         if match :
-          print 'French institution'
+          print 'French institution, appending to ../url_french_institutions.txt'
+          with open("../url_french_institutions.txt","a") as myfile:
+              myfile.write(institution_url + " : " + institution + "\n")
           for member_url in response.xpath('//h5[@class="ga-top-coauthor-name"]/a/@href').extract():
               complete_url = 'https://www.researchgate.net/' + member_url
               request = scrapy.Request(complete_url, callback=self.parse_member)
               request.meta['institution'] = institution
+              request.meta['institution_url'] = institution_url
               request.meta['member'] = response.xpath('//h5[@class="ga-top-coauthor-name"]/a[@href="' + member_url +'"]/text()').extract_first()
               request.meta['location'] = location
               yield request
           next_page = response.xpath('//a5[@class=" navi-next pager-link ajax-page-load"]/@href').extract_first()
           if next_page is not None:
               request = scrapy.Request(response.urljoin(next_page), callback=self.parse_institution_members)
-              request.meta['institution'] = institution
-              request.meta['location'] = location
               yield request
         
     def parse_member(self, response):
         print "we are in parse_member"
+        with open('www.researchgate.net/profile/' + response.url.split('/')[-1], "w") as myfile:
+            myfile.write(response.body)
         expertise = response.xpath('//a[@class="keyword-list-token-text ga-keyword-pill"]/text()').extract()
         yield {
             'institution' : response.meta['institution'],
+            'institution_url' : response.meta['institution_url'],
             'member' : response.meta['member'],
             'member_url' : response.url,
             'location' : response.meta['location'],
